6370bb63a3d577fbeaec6d9ad1a5ff25ccd6e7fc
Armando Gonzalez <iam@armando.mx>
2017-03-01 14:26:27 -0600

    linter support

--- index.js
+++ index.js
@@ -1,63 +1,70 @@
-var tSensor = require('bindings')('TemperatureSensor')
-var EventEmitter = require('events').EventEmitter;
-var inherits = require('util').inherits;
+const TSensor = require('bindings')('TemperatureSensor');
+const EventEmitter = require('events').EventEmitter;
+const inherits = require('util').inherits;
 
-function TemperatureSensor(_port, _add = 0){
+/**
+ * Creates an instance of temperature.
+ * @param {int} _port The port number where this component us connected.
+ * @param {int} _add The second argument.
+ * @returns {int} The sum of the two numbers.
+ */
+function TemperatureSensor(_port, _add = 0) {
+  const self = this;
   EventEmitter.call(this);
-  var _self = this;
+  this.temp = new TSensor(_port, _add);
 
-  this.temp = new tSensor(_port, _add);
-
-  process.on('SIGINT', function () {
-    _self.temp.release();
+  process.on('SIGINT', () => {
+    self.temp.release();
   });
 
-  process.on('SIGTERM', function () {
-    _self.temp.release();
+  process.on('SIGTERM', () => {
+    self.temp.release();
   });
 }
 
-TemperatureSensor.prototype.getValue = function(){
+TemperatureSensor.prototype.getValue = function getValue() {
   return this.temp.getValue();
-}
+};
 
-TemperatureSensor.prototype.getBasicValue = function(){
-  var value = Math.round(this.temp.getValue() * 100)/100;
+TemperatureSensor.prototype.getBasicValue = function getBasicValue() {
+  const value = Math.round(this.temp.getValue() * 100) / 100;
   return value;
-}
+};
 
-TemperatureSensor.prototype.getIntValue = function(){
+TemperatureSensor.prototype.getIntValue = function getIntValue() {
   return this.temp.getIntValue();
-}
+};
 
-TemperatureSensor.prototype.enableEvents = function () {
-  var _self = this;
-  var value;
+TemperatureSensor.prototype.enableEvents = function enableEvents() {
+  const self = this;
+  let value;
   if (!this.event_interval) {
-    this.event_interval = setInterval(()=>{
+    this.event_interval = setInterval(() => {
       value = this.getBasicValue();
-      _self.emit('medicion',value);
+      self.emit('medicion', value);
     }, 500); // 500ms muestreo
   }
-}
+};
 
-TemperatureSensor.prototype.when = function(value, callback){
+TemperatureSensor.prototype.when = function when(value, callback) {
   if (!this.interval) {
-    this.interval = setInterval(()=>{ // Tomar mediciones cada 200ms
-      console.log('Temperatura: ' + this.temp.getIntValue());
+    this.interval = setInterval(() => { // Tomar mediciones cada 200ms
+      /* eslint-disable no-console */
+      console.log(`Temperatura: ${this.temp.getIntValue()}`);
+      /* eslint-disable eqeqeq */
       if (this.temp.getIntValue() == value) {
         callback();
       }
     }, 500); // Tomar mediciones cada 500ms
   }
-}
+};
 
-TemperatureSensor.prototype.release = function() {
+TemperatureSensor.prototype.release = function release() {
   clearInterval(this.interval);
   clearInterval(this.event_interval);
   this.temp.release();
-}
+};
 
-inherits(TemperatureSensor,EventEmitter);
+inherits(TemperatureSensor, EventEmitter);
 
 module.exports = TemperatureSensor;
