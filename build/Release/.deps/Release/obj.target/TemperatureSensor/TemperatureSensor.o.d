82bd011d2370ec2bfeb5f53cdd745bce92bb3760
Armando Gonzalez <iam@armando.mx>
2017-03-02 13:16:40 -0600

    fixed comments

--- build/Release/.deps/Release/obj.target/TemperatureSensor/TemperatureSensor.o.d
+++ build/Release/.deps/Release/obj.target/TemperatureSensor/TemperatureSensor.o.d
@@ -1,20 +1,22 @@
-cmd_Release/obj.target/TemperatureSensor/TemperatureSensor.o := g++ '-DNODE_GYP_MODULE_NAME=TemperatureSensor' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/pi/.node-gyp/6.5.0/include/node -I/home/pi/.node-gyp/6.5.0/src -I/home/pi/.node-gyp/6.5.0/deps/uv/include -I/home/pi/.node-gyp/6.5.0/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/TemperatureSensor/TemperatureSensor.o.d.raw   -c -o Release/obj.target/TemperatureSensor/TemperatureSensor.o ../TemperatureSensor.cpp
+cmd_Release/obj.target/TemperatureSensor/TemperatureSensor.o := c++ '-DNODE_GYP_MODULE_NAME=TemperatureSensor' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/Users/manduks/.node-gyp/7.4.0/include/node -I/Users/manduks/.node-gyp/7.4.0/src -I/Users/manduks/.node-gyp/7.4.0/deps/uv/include -I/Users/manduks/.node-gyp/7.4.0/deps/v8/include  -Os -gdwarf-2 -mmacosx-version-min=10.7 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++0x -stdlib=libc++ -fno-rtti -fno-exceptions -fno-threadsafe-statics -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/TemperatureSensor/TemperatureSensor.o.d.raw   -c -o Release/obj.target/TemperatureSensor/TemperatureSensor.o ../TemperatureSensor.cpp
 Release/obj.target/TemperatureSensor/TemperatureSensor.o: \
- ../TemperatureSensor.cpp /home/pi/.node-gyp/6.5.0/include/node/node.h \
- /home/pi/.node-gyp/6.5.0/include/node/v8.h \
- /home/pi/.node-gyp/6.5.0/include/node/v8-version.h \
- /home/pi/.node-gyp/6.5.0/include/node/v8config.h \
- /home/pi/.node-gyp/6.5.0/include/node/node_version.h ../TempWrapper.h \
- /home/pi/.node-gyp/6.5.0/include/node/node_object_wrap.h \
- ../src/Modules/AnalogModules/TemperatureSensor.h \
- ../src/Modules/AnalogModules/../../Libraries/ADS1015/ADS1015.h
+  ../TemperatureSensor.cpp \
+  /Users/manduks/.node-gyp/7.4.0/include/node/node.h \
+  /Users/manduks/.node-gyp/7.4.0/include/node/v8.h \
+  /Users/manduks/.node-gyp/7.4.0/include/node/v8-version.h \
+  /Users/manduks/.node-gyp/7.4.0/include/node/v8config.h \
+  /Users/manduks/.node-gyp/7.4.0/include/node/node_version.h \
+  ../TempWrapper.h \
+  /Users/manduks/.node-gyp/7.4.0/include/node/node_object_wrap.h \
+  ../src/Modules/AnalogModules/TemperatureSensor.h \
+  ../src/Modules/AnalogModules/../../Libraries/ADS1015/ADS1015.h
 ../TemperatureSensor.cpp:
-/home/pi/.node-gyp/6.5.0/include/node/node.h:
-/home/pi/.node-gyp/6.5.0/include/node/v8.h:
-/home/pi/.node-gyp/6.5.0/include/node/v8-version.h:
-/home/pi/.node-gyp/6.5.0/include/node/v8config.h:
-/home/pi/.node-gyp/6.5.0/include/node/node_version.h:
+/Users/manduks/.node-gyp/7.4.0/include/node/node.h:
+/Users/manduks/.node-gyp/7.4.0/include/node/v8.h:
+/Users/manduks/.node-gyp/7.4.0/include/node/v8-version.h:
+/Users/manduks/.node-gyp/7.4.0/include/node/v8config.h:
+/Users/manduks/.node-gyp/7.4.0/include/node/node_version.h:
 ../TempWrapper.h:
-/home/pi/.node-gyp/6.5.0/include/node/node_object_wrap.h:
+/Users/manduks/.node-gyp/7.4.0/include/node/node_object_wrap.h:
 ../src/Modules/AnalogModules/TemperatureSensor.h:
 ../src/Modules/AnalogModules/../../Libraries/ADS1015/ADS1015.h:
