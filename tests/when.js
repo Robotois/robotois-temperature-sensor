a444898a2e1adbeafed226553d85ba227ea2cd1b
Armando Gonzalez <iam@armando.mx>
2017-03-02 17:05:53 -0600

    resolve lint issues testing

--- tests/when.js
+++ tests/when.js
@@ -1,23 +1,18 @@
-var _temp = require('../index'),
-  temp = new _temp(1);
+const Temperature = require('../index');
 
-temp.when(25,()=>{
-  console.log("Wow, la temperatura es 25 C!!");
+const temp = new Temperature(1);
+
+temp.when(25, () => {
+  /* eslint-disable no-console */
+  console.log('Wow, la temperatura es 25 C!!');
 });
 
-// setInterval(()=>{ // Proceso en estado ocioso
-//   console.log("Temp: " + temp.getValue().toFixed(3));
-//   console.log("Int: " + temp.getIntValue());
-// },1000);
+setInterval(() => {}, 10000);
 
-setInterval(()=>{ // Proceso en estado ocioso
-  true;
-},10000);
-
-process.on('SIGTERM', function () {
+process.on('SIGTERM', () => {
   process.exit();
 });
 
-process.on('SIGINT', function () {
+process.on('SIGINT', () => {
   process.exit();
 });
