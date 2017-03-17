9cbbfd9ee956209a48108fde56ba86985e7d62a8
yovany-lg <yovany@gmail.com>
2017-03-17 21:16:05 +0000

    Release method fixes

--- tests/when.js
+++ tests/when.js
@@ -1,29 +1,13 @@
-<<<<<<< HEAD
-const TempSensor = require('../index');
-
-const temp = new TempSensor(1);
-
-temp.when(25, () => {
-  console.log('Wow, la temperatura es 25 C!!');
-});
-=======
 const Temperature = require('../index');
 
 const temp = new Temperature(1);
->>>>>>> 7fcada0cf88bb1578ff1b24c2c11bbddb8749e61
 
 temp.when(25, () => {
   /* eslint-disable no-console */
   console.log('Wow, la temperatura es 25 C!!');
 });
 
-<<<<<<< HEAD
-setInterval(() => { // Proceso en estado ocioso
-  // true;
-}, 10000);
-=======
 setInterval(() => {}, 10000);
->>>>>>> 7fcada0cf88bb1578ff1b24c2c11bbddb8749e61
 
 process.on('SIGTERM', () => {
   process.exit();
